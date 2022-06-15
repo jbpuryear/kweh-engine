09c2df3c943c6a667babca845b17f423326756b0
James Puryear <jpuryear@gmx.com>
2022-06-14 23:54:19 -0400

    Renderer texture create, delete functions

--- src/render/Renderer.js
+++ src/render/Renderer.js
@@ -11,6 +11,7 @@ export default class Renderer extends EventEmitter {
     this._activeAttributes = new Set();
     this._buffer = new ArrayBuffer(bufferSize);
     this._glBuffer = null;
+    this._lastBoundTexture = null;
     this._views = new Map([
       [ 'Int8', new Int8Array(this._buffer) ],
       [ 'Uint8', new Uint8Array(this._buffer) ],
@@ -46,8 +47,11 @@ export default class Renderer extends EventEmitter {
 
   destroy() {
     this.clear();
-    this.gl = null;
+    this.gl.deleteBuffer(this._glBuffer);
+    this._buffer = null;
     this._pipeline = null;
+    this._lastBoundTexture = null;
+    this.gl = null;
   }
 
 
@@ -89,6 +93,32 @@ export default class Renderer extends EventEmitter {
 
   deleteProgram(program) {
     this.gl.deleteProgram(program);
+  }
+
+
+  createTexture(source) {
+    const gl = this.gl;
+    const glTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, glTexture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, source);
+
+    // Are dimensions powers of 2?
+    if ((source.width & (source.width - 1) === 0) && (source.height & (source.height - 1 === 0))) {
+      gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+
+    gl.bindTexture(gl.TEXTURE_2D, this._lastBoundTexture);
+
+    return glTexture;
+  }
+
+
+  deleteTexture(glTexture) {
+    this.gl.deleteTexture(glTexture);
   }
 
 
