d4b7effb709de91611c606d53906ff14531e1e74
James Puryear <jpuryear@gmx.com>
2022-08-04 00:56:22 -0400

    AABB does not overlap if only edges touch

--- src/math/AABB.js
+++ src/math/AABB.js
@@ -29,10 +29,10 @@ export default class AABB {
 
   static overlaps(a, b) {
     return (
-      a.x <= b.x + b.width &&
-      a.x + a.width >= b.x &&
-      a.y <= b.y + b.height &&
-      a.y + a.height >= b.y
+      a.x < b.x + b.width &&
+      a.x + a.width > b.x &&
+      a.y < b.y + b.height &&
+      a.y + a.height > b.y
     );
   }
 
