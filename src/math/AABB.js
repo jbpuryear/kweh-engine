3ea3d8ab931a87b0f965232d960c7ebf6a113b93
James Puryear <jpuryear@gmx.com>
2022-08-22 18:35:24 -0400

    Add various AABB methods

--- src/math/AABB.js
+++ src/math/AABB.js
@@ -27,10 +27,37 @@ export default class AABB {
   }
 
 
+  static expand(aabb, x, y, out) {
+    out.x = aabb.x - x;
+    out.y = aabb.y - y;
+    out.width = aabb.width + x * 2;
+    out.height = aabb.height + y * 2;
+    return out;
+  }
+
+
   static getRandomPoint(aabb, out) {
     out.x = aabb.x + Math.random() * aabb.width;
     out.y = aabb.y + Math.random() * aabb.height;
     return out;
+  }
+
+
+  static merge(a, b, out) {
+    out.x = Math.min(a.x, b.x);
+    out.y = Math.min(a.y, b.y);
+    out.width = Math.max(a.x + a.width, b.x + b.width) - out.x;
+    out.height = Math.max(a.y + a.height, b.y + b.height) - out.y;
+    return out;
+  }
+
+
+  static mergePoint(aabb, x, y, out) {
+    const x0 = Math.min(aabb.x, x);
+    const x1 = Math.max(aabb.x + aabb.width, x);
+    const y0 = Math.min(aabb.y, y);
+    const y1 = Math.max(aabb.y + aabb.height, y);
+    return AABB.set(out, x0, y0, x1 - x0, y1 - y0);
   }
 
 
@@ -55,8 +82,11 @@ export default class AABB {
 
   contains(x, y) { return AABB.contains(this, x, y); }
   copy(aabb) { return AABB.copy(aabb, this); }
+  expand(x, y) { return AABB.expand(this, x, y, this); }
   getRandomPoint(v) { return AABB.getRandomPoint(this, v); }
   overlaps(aabb) { return AABB.overlaps(this, aabb); }
+  merge(aabb) { return AABB.merge(this, aabb, this); }
+  mergePoint(x, y) { return AABB.mergePoint(this, x, y, this); }
   set(x, y, w, h) { return AABB.set(this, x, y, w, h); }
 
   left() { return this.x; }
